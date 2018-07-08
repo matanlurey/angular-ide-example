3ff983bb9743b1be047403eb98942d38561bc147
Matan Lurey <matan@lurey.org>
2018-07-07 20:02:29 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+An example of using `angular 5.0.0-beta`.
+
+```bash
+$ pub upgrade
+$ pub run build_runner serve
+```
---
+++ web/main.dart
@@ -0,0 +1,13 @@
+import 'package:angular/angular.dart';
+
+import 'main.template.dart' as ng;
+
+void main() {
+  runApp(ng.HelloWorldComponentNgFactory);
+}
+
+@Component(
+  selector: 'hello-world',
+  template: 'Hello World!',
+)
+class HelloWorldComponent {}
